1e5cbf758f8b1ef57dcf0d50a3c42db2ec7aeb65
Olga <olga_hom@mail.ru>
2022-12-04 13:19:45 +0300

    Task2 HW3

--- HomeWork3/Task1.py
+++ HomeWork3/Task1.py
@@ -3,11 +3,28 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 import random
-from functions import fill_list
+from functions import fill_list, List
 # num = int(input('Введите чило: '))
 numbers = fill_list()
-sum = 0
-for i in range(1, len(numbers), 2):
-    sum += numbers[i]
+
+def summing_odd_list_items (nums_list: List[int]) -> List:
+
+  '''
+  Возвращает список, состоящий из сумм нечетных элементов исходного списка
+  
+  Args:
+    List[int] - Список числел
+  Reterns:
+    List[int] - Cписок сумм числел
+  '''
+
+  sum = 0
+
+  for i in range(1, len(numbers), 2):
+      sum += numbers[i]
+  return sum
+
+result = summing_odd_list_items(numbers)
+
 print(f'Список элементов {numbers}')
-print(f'Сумма нечетных элементов списка равна {sum}')
+print(f'Сумма нечетных элементов списка равна {result}')
--- HomeWork3/Task2.py
+++ HomeWork3/Task2.py
@@ -1,5 +1,28 @@
-# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 # Пример:
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
+from functions import fill_list, List
+import math
+
+
+def mult_pairs_list_items(nums_list: List[int]) -> List:
+    '''
+    Возвращает список, состоящий из произведений пар элементов исходного списка
+    Пара элементов это первый и последний элемент, второй и предпоследний и т.д.
+
+    Args:
+      List[int] - Список числел
+    Reterns:
+      List[int] - Cписок произведений числел
+    '''
+    mult_list = []
+    for i in range(math.ceil(len(numbers)/2)):
+        mult_list.append(nums_list[i] * nums_list[-(i+1)])
+    return mult_list
+
+numbers = fill_list()
+result = mult_pairs_list_items(numbers)
+print(f'Список элементов {numbers}')
+print(f'Произведение пар элементов списка равна {result}')
